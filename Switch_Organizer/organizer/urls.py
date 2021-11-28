59a36303fe005a6cffc0c46cfcc8887cd641f7a1
Brett Andrews <brettandrews3@gmail.com>
2021-11-28 15:33:22 -0600

    Resolved the error mentioned before where the server wouldn't launch. Currently getting 404 error on 127.0.0.1:8000. Empty path doesn't match organizer/ or admin/.

--- Switch_Organizer/organizer/urls.py
+++ Switch_Organizer/organizer/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 # urlpatterns simplifies the web address system for the app
+
 urlpatterns = [
-	path('', views.index, name='index'),
+	path('', views.index, name='index')
 ]
