1faa5dd36cdf4a1de2ef106355d1332236e509e1
Brett <pi@raspberrypi.attlocal.net>
2021-12-13 16:17:56 -0600

    Added classes VideoGame and Choice to models.py. The 'question' var in class Choice needs to be modified to match class VideoGame. Next: add 'organizer' app to INSTALLED_APPS under settings.py.

--- Switch_Organizer/organizer/models.py
+++ Switch_Organizer/organizer/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class VideoGame(models.Model):
+	"""This class allows the user to enter the standard data for their
+	video games. It starts with the basics: name, release date, publisher,
+	genre, console. Other fields may be added later on.
+	"""
+	game_name = models.CharField(max_length=200)
+	release_date = models.DateTimeField('date released')
+	publisher = models.CharField(max_length=50)
+	genre = models.CharField(max_length=200)
+	game_console = models.CharField(max_length=50)
+
+class Choice(models.Model):
+	"""TODO: This class will allow for reviews and ratings by the time of project
+	completion. Users will be able to save their thoughts on the game and rate
+	the game on a scale of 1-10. For this initial code draft, I'm using the 
+	Django poll tutorial's fields as placeholders while I figure this out.
+	"""
+	question = models.ForeignKey(Question, on_delete=models.CASCADE)
+	choice_text = models.CharField(max_length=1000)
+	votes = models.IntegerField(default=0)
