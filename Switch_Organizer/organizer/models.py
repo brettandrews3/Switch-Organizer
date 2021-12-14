43661d88e8e264d937f532a7f36125cdc27ac852
Brett <brettandrews3@gmail.com>
2021-12-14 11:27:47 -0600

    Cleaned up the help notes for class Review. Changed rating to have a default of 1 instead of 0. Will change it back later if needed.

--- Switch_Organizer/organizer/models.py
+++ Switch_Organizer/organizer/models.py
@@ -13,14 +13,15 @@ class VideoGame(models.Model):
 	game_console = models.CharField(max_length=50)
 
 class Review(models.Model):
-	"""TODO: This class will allow for reviews and ratings by the time of project
-	completion. Users will be able to save their thoughts on the game and rate
-	the game on a scale of 1-10. For this initial code draft, I'm using the 
-	Django poll tutorial's fields as placeholders while I figure this out.
+	"""This class will allow for reviews and ratings. Users will be able to save their 
+	thoughts on the game and rate the game on a scale of 1-10. The var game holds the
+	ForeignKey and links Review class to VideoGame class. review_text generates the text
+	box for users to write brief reviews of the games. Finally, rating will allow for
+	a numerical score of 1-10 for the game being reviewed.
 	"""
 	game = models.ForeignKey(VideoGame, on_delete=models.CASCADE)
 	review_text = models.CharField(max_length=1000)
-	rating = models.IntegerField(default=0)
+	rating = models.IntegerField(default=1)
 
 """
 class Choice(models.Model):
