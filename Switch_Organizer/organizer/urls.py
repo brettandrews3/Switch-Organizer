e4570037281b54ab758658f400c90f68a257183a
Brett <brettandrews3@gmail.com>
2021-12-28 11:59:20 -0600

    Modified index.html to point at namespaced detail.html view. Added missing /results and /rating to respective paths on urls.py. Next: begin work on form processing.

--- Switch_Organizer/organizer/urls.py
+++ Switch_Organizer/organizer/urls.py
@@ -3,13 +3,14 @@ from . import views
 
 # urlpatterns simplifies the web address system for the app
 
+app_name = 'organizer'
 urlpatterns = [
 	# ex: /organizer/
 	path('', views.index, name='index'),
 	# ex: /organizer/3/
 	path('<int:game_id>/', views.detail, name='detail'),
 	# ex: /organizer/3/results/
-	path('<int:game_id>/', views.results, name='results'),
+	path('<int:game_id>/results', views.results, name='results'),
 	# ex: /organizer/3/rating/
-	path('<int:game_id>/', views.rating, name='rating'),
+	path('<int:game_id>/rating', views.rating, name='rating'),
 ]
