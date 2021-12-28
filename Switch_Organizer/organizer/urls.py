c67d16eb1e740f457c797f70302a1e015a711741
Brett <brettandrews3@gmail.com>
2021-12-28 10:13:19 -0600

    Added standardized views and coordinated them with urlpatterns in the urls.py section. New views work properly on server. Next: add index view, create template dir in /organizer/.

--- Switch_Organizer/organizer/urls.py
+++ Switch_Organizer/organizer/urls.py
@@ -4,5 +4,12 @@ from . import views
 # urlpatterns simplifies the web address system for the app
 
 urlpatterns = [
-	path('', views.index, name='index')
+	# ex: /organizer/
+	path('', views.index, name='index'),
+	# ex: /organizer/3/
+	path('<int:game_id>/', views.detail, name='detail'),
+	# ex: /organizer/3/results/
+	path('<int:game_id>/', views.results, name='results'),
+	# ex: /organizer/3/rating
+	path('<int:game_id>/', views.rating, name='rating'),
 ]
--- Switch_Organizer/organizer/views.py
+++ Switch_Organizer/organizer/views.py
@@ -7,3 +7,15 @@ def index(request):
 	"""This function creates the index for the organizer app.
 	"""
 	return HttpResponse("Hello, friendo. You're at the organizer index.")
+
+# Here's the standardized HttpResponses for urlpatterns in urls.py:
+
+def detail(request, game_id):
+	return HttpResponse("You're looking at game %s." % game_id)
+
+def results(request, game_id):
+	response = "You're looking at the results of game %s."
+	return HttpResponse(response % game_id)
+
+def rating(request, game_id):
+	return HttpResponse("You're rating game %s." % game_id)
