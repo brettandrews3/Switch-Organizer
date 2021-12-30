f8a964a398279587d6d35ad1ddee2a0ed3434b5b
Brett <brettandrews3@gmail.com>
2021-12-30 17:48:56 -0600

    Just wrote first versions of results.html and changed def results to alter template name. About to migrate to server and test app for debugging.

--- Switch_Organizer/organizer/views.py
+++ Switch_Organizer/organizer/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+
 from .models import VideoGame
 
 # Create your views here.
@@ -28,3 +30,22 @@ def results(request, game_id):
 
 def rating(request, game_id):
 	return HttpResponse("You're rating game %s." % game_id)
+
+def rate(request, game_id):
+	game = get_object_or_404(VideoGame, pk=game_id)
+	try:
+		selected_review = game.review_set.get(pk=request.POST['review'])
+	except (KeyError, Review.DoesNotExist):
+		# Should redisplay the rating form:
+		return render(request, 'organizer/detail.html', {
+			'game': game,
+			'error_message': "You didn't select a choice.",
+			})
+	else:
+		selected_review.recommends += 1 # In text recommends = 'votes' here
+		selected_review.save()
+		"""Return HttpResponseRedirect after dealing with the POST data
+		successfully. This will prevent data from getting posted twice if
+		user hits the Back button on their browser.
+		"""
+		return HttpResponseRedirect(reverse('reviews:results', args=(game.id,)))
