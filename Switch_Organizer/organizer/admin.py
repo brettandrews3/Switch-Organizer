a029ef60c6fda009efe603c2d472980bab409338
Brett <brettandrews3@gmail.com>
2022-01-04 12:24:04 -0600

    Admin is taking shape. Added in class Review and made the review section compact with TabularInline. Next: fill out the admin change list to allow for changes to the various fields for each game.

--- Switch_Organizer/organizer/admin.py
+++ Switch_Organizer/organizer/admin.py
@@ -1,14 +1,19 @@
 from django.contrib import admin
 
-from .models import VideoGame
+from .models import VideoGame, Review
 
 # Register your models here.
+
+class ReviewInline(admin.TabularInline):
+	model = Review
+	extra = 3
 
 class VideoGameAdmin(admin.ModelAdmin):
 	fieldsets = [
 		(None, {'fields': ['game_name']}),
-		('Date information', {'fields': ['pub_date']}),
+		('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
 	]
+	inlines = [ReviewInline]
 
 admin.site.register(VideoGame, VideoGameAdmin)
 #admin.site.register(VideoGame)
