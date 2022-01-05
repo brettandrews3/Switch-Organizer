4d5eca7b59cd6f436d661d83dd9015b6a089e2f2
Brett <brettandrews3@gmail.com>
2022-01-05 11:10:27 -0600

    Deleted commented-out old version of class views. Next: create branch to test sorting organizer by game_name and game_console.

--- Switch_Organizer/organizer/views.py
+++ Switch_Organizer/organizer/views.py
@@ -35,32 +35,6 @@ class ResultsView(generic.DetailView):
 	model = VideoGame
 	template_name = 'organizer/results.html'
 
-"""
-def index(request):
-	latest_games_added = VideoGame.objects.order_by('-pub_date')[:5]
-	template = loader.get_template('organizer/index.html')
-	context = {'latest_games_added': latest_games_added}
-	return render(request, 'organizer/index.html', context)
-	#return HttpResponse(template.render(context, request))
-	#output = ', '.join([v.game_name for v in latest_games_added])
-	#return HttpResponse(output)
-	#return HttpResponse("Hello, friendo. You're at the organizer index.")
-
-
-# Here's the standardized HttpResponses for urlpatterns in urls.py:
-def detail(request, game_id):
-	game = get_object_or_404(VideoGame, pk=game_id)
-	return render(request, 'organizer/detail.html', {'game': game})
-
-def results(request, game_id):
-	game = get_object_or_404(VideoGame, pk=game_id)
-	return render(request, 'organizer/results.html', {'game': game})
-"""
-
-"""def results(request, game_id):
-	response = "You're looking at the results of game %s."
-	return HttpResponse(response % game_id)"""
-
 def rating(request, game_id):
 	return HttpResponse("You're rating game %s." % game_id)
 
@@ -77,7 +51,8 @@ def rate(request, game_id):
 	else:
 		selected_review.recommends += 1 # In text recommends = 'votes' here
 		selected_review.save()
-		"""Return HttpResponseRedirect after dealing with the POST data
+		"""
+		Return HttpResponseRedirect after dealing with the POST data
 		successfully. This will prevent data from getting posted twice if
 		user hits the Back button on their browser.
 		"""
